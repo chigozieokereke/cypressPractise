537df4c4841b0a64d82328e042bc00dcd3b34ecc
Chigozie Okereke <chigozie.okereke@seera.sa>
2022-03-09 14:34:20 +0400

    session 4-draft

--- cypress/fixtures/login/login_actions.js
+++ cypress/fixtures/login/login_actions.js
@@ -1,9 +1,10 @@
 import login_po from "../login/login_po";
 import testConst from "../constants";
 
-export function loginWithEmail() {
-  cy.get(login_po.emailLoginField).type(testConst.loginEmail);
-  cy.get(login_po.passwordLoginField).type(testConst.loginPassword);
+export function loginWithEmail(email,password) {
+  cy.get(login_po.headerSigninButton).click();
+  cy.get(login_po.emailLoginField).type(email);
+  cy.get(login_po.passwordLoginField).type(password);
   cy.get(login_po.signinButton).click({ force: true });
 }
 
